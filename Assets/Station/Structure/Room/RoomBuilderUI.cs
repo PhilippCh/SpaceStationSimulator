b064a768b9f66b8d936fab4ef2ca817abff2fb02
Philipp Christoph <philipp.christoph@dm.de>
2015-08-16 22:23:52 +0200

    refactor(room): finalise build mode refactor

--- Assets/Station/Structure/Room/RoomBuilderUI.cs
+++ Assets/Station/Structure/Room/RoomBuilderUI.cs
@@ -8,32 +8,65 @@ using UnityEngine.UI;
 
 namespace SpaceStation.Station.Structure.Room {
 
-	/* BEGIN OLD DEFINITIONS */
-
+	/* TODO: Unify this with Station.Structure.Cell.CellType */
 	public enum CellType {
 		EMPTY,
 		FLOOR,
 		WALL
 	}
 
-	public class RoomDefinition {
-		public Rect bounds;
-		public CellType[,] cells;
-	}
-
-	/* END OLD DEFINITIONS */
-
 	public class RoomBuilderUI : MonoBehaviour {
 
+		private struct MouseData {
+			public IntVector2 originPosition;
+			public IntVector2 currentPosition;
+			public IntVector2 lastPosition;
+
+			public bool HasMoved() {
+				return currentPosition.x != lastPosition.x || currentPosition.z != lastPosition.z;
+			}
+
+			public bool isPressed;
+		}
+
+		private enum SelectionMode {
+			ROOM,
+			SINGLE
+		}
+
 		public int TileSize = 24;
-		public int BuildArea = 64;
+		public int AreaSize = 16;
 
 		private Dictionary<CellType, GUIStyle> styles;
+
+		private CellType[,] cells;
+
+		private CellType[,] temporaryCells;
+		private IntVector2 temporaryOrigin;
+		private IntVector2 temporarySize;
+
+		private MouseData mouseData;
 
 		public RoomBuilderUI() {
 			styles = new Dictionary<CellType, GUIStyle>();
 		}
 
+		/**
+		 * Prepares all UI to show the room editing dialog.
+		 * This should be the central entry point for all other scripts referencing the room builder.
+		 * 
+		 * @param center - Center point of the editable region
+		 */
+		public void ShowBuildDialog(IntVector3 center) {
+			this.cells = new CellType[AreaSize, AreaSize];
+			this.temporaryCells = null;
+		}
+
+		/**
+		 * Creates a simple 1x1 texture containing a single color value.
+		 * 
+		 * @param color - Color for the texture
+		 */
 		private Texture2D CreateColorTexture(Color color) {
 			var texture = new Texture2D(1, 1);
 			
@@ -43,12 +76,22 @@ namespace SpaceStation.Station.Structure.Room {
 			return texture;
 		}
 
+		/**
+		 * Sets up basic styles for visualization of different tile types.
+		 */
 		private void SetupStyles() {
 			CreateSimpleStyle(CellType.WALL, Color.black, Color.yellow);
 			CreateSimpleStyle(CellType.FLOOR, Color.black, Color.green);
 			CreateSimpleStyle(CellType.EMPTY, Color.white, Color.blue);
 		}
 
+		/**
+		 * Creates a simple style for a given cell type.
+		 * 
+		 * @param cellType - Cell type to create the style for
+		 * @param textColor - Text color of the tile
+		 * @param backgroundColor - Background color of the tile
+		 */
 		private void CreateSimpleStyle(CellType cellType, Color textColor, Color backgroundColor) {
 			var style = new GUIStyle(GUI.skin.GetStyle("Label"));
 
@@ -59,201 +102,142 @@ namespace SpaceStation.Station.Structure.Room {
 			styles.Add(cellType, style);
 		}
 
-		/* OLD PART BEGINS HERE */
+		/**
+		 * Draws a single tile at the specified coordinate multiplied by tile size.
+		 * 
+		 * @param x - X coordinate of the tile
+		 * @param z - z coordinate of the tile
+		 * @param cellType - Type of the cell
+		 */
+		private void DrawTile(int x, int z, CellType cellType) {
+			var rect = new Rect(x * TileSize, z * TileSize, TileSize, TileSize);
+			var formattedName = cellType.ToString().Substring(0, 1);
 
-		Vector2 originalPos, currentPos;
-		Vector2 prevPos = new Vector2(-1, -1);
-		bool drawing = false;
-		bool wasDown = false;
-		bool finished = false;
+			GUI.Label(rect, formattedName, styles[cellType]);
+		}
 
-		IntVector2 size;
+		/**
+		 * Converts screen space to tile grid coordinates.
+		 * 
+		 * @param original - Screen space coordinates
+		 */
+		private IntVector2 ConvertScreenToTileCoords(Vector3 original) {
+			var coordinates = new IntVector2(original.x, original.y);
 
-		CellType[,] cells;
+			coordinates.x = coordinates.x / TileSize;
+			coordinates.z = (Screen.height - coordinates.z) / TileSize;
 
-		List<RoomDefinition> rooms = new List<RoomDefinition>();
-		RoomDefinition overlayRoom;
+			return coordinates;
+		}
 
+		private void SetCells(IntVector2 origin, CellType[,] cells) {
+
+			for (int x = 0; x < cells.GetLength(0); x++) {
+				for (int z = 0; z < cells.GetLength(1); z++) {
+					var spacePosition = new IntVector2(origin.x + x, origin.z + z);
+
+					if (spacePosition.x > 0 && spacePosition.x < AreaSize && 
+					    spacePosition.z > 0 && spacePosition.z < AreaSize) {
+
+						this.cells[spacePosition.x, spacePosition.z] = cells[x, z];
+					}
+				}
+			}
+		}
+
+		/**
+		 * Resets cell data.
+		 */
+		public void Reset() {
+			ShowBuildDialog(new IntVector3(128, 128, 128));
+			
+			Logger.Info("Reset", "Reset room drawing");
+		}
+
+		/**
+		 * Initializes builder dialog.
+		 */
+		private void Awake() {
+			ShowBuildDialog(new IntVector3(128, 128, 128));
+		}
+
+		/**
+		 * Renders all builder-related UI
+		 */
 		private void OnGUI() {
-
 			if (styles.Count == 0) {
 				SetupStyles();
 			}
 
-			// Draw existing temporary rooms
-			foreach (RoomDefinition room in rooms) {
-				DrawRoom(new IntVector2((int) room.bounds.x, (int)room.bounds.y), room.cells);
-			}
-
-			// Draw current temporary room
-			if (drawing) {
-				Vector2 usePos = originalPos;
-
-				GUI.Label(new Rect(currentPos.x * TileSize, currentPos.y * TileSize, 150, 20), currentPos.ToString());
-				GUI.Label(new Rect(currentPos.x * TileSize, currentPos.y * TileSize + 20, 150, 20), originalPos.ToString());
-
-				if (currentPos.x < originalPos.x) {
-					usePos.x = currentPos.x;
-				}
-
-				if (currentPos.y < originalPos.y) {
-					usePos.y = currentPos.y;
-				}
-
-				DrawRoom(new IntVector2(usePos.x, usePos.y), cells);
-			}
-
-			if (overlayRoom != null) {
-				var adjustedBounds = new Rect(overlayRoom.bounds.position * TileSize, overlayRoom.bounds.size * TileSize);
-				var buttonPos = new Rect(adjustedBounds.position + adjustedBounds.size / 2, new Vector2(40, 30));
-
-				buttonPos.x -= 20;
-				buttonPos.y -= 15;
-
-				if (GUI.Button(buttonPos, "X")) {
-					rooms.Remove(overlayRoom);
+			/* Draw editable area */
+			for (int x = 0; x < this.cells.GetLength(0); x++) {
+				for (int z = 0; z < this.cells.GetLength(1); z++) {
+					DrawTile(x, z, this.cells[x, z]);
 				}
 			}
 
-			if (finished) {
-				DrawRoom(IntVector2.zero, cells);
-			}
-		}
+			/* Draw temporary room (if present) */
+			if (this.temporaryCells != null) {
 
-		private void DrawRoom(IntVector2 origin, CellType[,] cells) {
-			if (cells == null) {
-				return;
-			}
-
-			for (int x = 0; x < cells.GetLength(0); x++) {
-				for (int z = 0; z < cells.GetLength(1); z++) {
-					var rect = new Rect((origin.x + x) * TileSize, (origin.z + z) * TileSize, TileSize, TileSize);
-					var formattedName = cells[x, z].ToString().Substring(0, 1);
-
-					GUI.Label(rect, formattedName, styles[cells[x, z]]);
+				for (int x = 0; x < this.temporaryCells.GetLength(0); x++) {
+					for (int z = 0; z < this.temporaryCells.GetLength(1); z++) {
+						DrawTile(temporaryOrigin.x + x, temporaryOrigin.z + z, this.temporaryCells[x, z]);
+					}
 				}
 			}
 		}
 
+		/**
+		 * Updates mouse input and triggers corresponding edit actions.
+		 */
 		private void Update() {
+
+			/* Set origin coordinates if new click was detected */
 			if (Input.GetMouseButtonDown(0)) {
-				originalPos = Input.mousePosition.ToVector2();
-				originalPos.x = Mathf.Floor(originalPos.x / TileSize);
-				originalPos.y = Mathf.Floor((Screen.height - originalPos.y) / TileSize);
-
-				wasDown = true;
-
-				if (finished) {
-					finished = false;
-					this.cells = null;
-				}
+				mouseData.originPosition = ConvertScreenToTileCoords(Input.mousePosition);
+				mouseData.isPressed = true;
 			}
 
-			drawing = Input.GetMouseButton(0);
-
+			/* Drag to create a temporary room (created upon release) */
 			if (Input.GetMouseButton(0)) {
+				mouseData.currentPosition = ConvertScreenToTileCoords(Input.mousePosition);
 
-				currentPos = Input.mousePosition.ToVector2();
-				currentPos.x = Mathf.Floor(currentPos.x / TileSize);
-				currentPos.y = Mathf.Floor((Screen.height - currentPos.y) / TileSize);
+				if (mouseData.HasMoved()) {
+					temporarySize.x = Mathf.Abs(mouseData.currentPosition.x - mouseData.originPosition.x);
+					temporarySize.z = Mathf.Abs(mouseData.currentPosition.z - mouseData.originPosition.z);
 
-				if (currentPos != prevPos) {
-					size = new IntVector2((int)Mathf.Abs(currentPos.x - originalPos.x), (int)Mathf.Abs(currentPos.y - originalPos.y));
-					
-					cells = new CellType[size.x, size.z];
+					this.temporaryCells = new CellType[temporarySize.x, temporarySize.z];
 
-					for (int x = 0; x < size.x; x++) {
-						for (int z = 0; z < size.z; z++) {
-							if (x == 0 || x == size.x - 1 || z == 0 || z == size.z - 1) {
-								cells[x, z] = CellType.WALL;
-							} else {
-								cells[x, z] = CellType.FLOOR;
-							}
+					for (int x = 0; x < temporarySize.x; x++) {
+						for (int z = 0; z < temporarySize.z; z++) {
+							var isWall = (x == 0 || x == temporarySize.x - 1 || z == 0 || z == temporarySize.z - 1);
+
+							this.temporaryCells[x, z] = isWall ? CellType.WALL : CellType.FLOOR;
 						}
 					}
-				}
+			
+					this.temporaryOrigin = mouseData.originPosition;
 
-				prevPos = currentPos;
-			} else if (!Input.GetMouseButton(0) && wasDown) {
-				if (Vector2.Distance(currentPos, originalPos) < 1) {
-					overlayRoom = null;
-					
-					foreach (RoomDefinition room in rooms) {
-						var mousePos = Input.mousePosition;
-						var adjustedBounds = new Rect(room.bounds.position * TileSize, room.bounds.size * TileSize);
-						
-						mousePos.y = Screen.height - mousePos.y;
-						
-						if (adjustedBounds.Contains(mousePos)) {
-							overlayRoom = room;
-						}
-					}
-
-					if (overlayRoom != null) {
-						Logger.Info("Update", "Selected room {0}", overlayRoom.bounds.ToString());
-					}
-				} else if (cells != null) {
-					var xOrigin = currentPos.x < originalPos.x ? currentPos.x : originalPos.x;
-					var zOrigin = currentPos.y < originalPos.y ? currentPos.y : originalPos.y;
-					
-					if (size.x != 0 && size.z != 0) {
-						// Assemble the room and add to list
-						AddTemporaryRoom((int) xOrigin, (int) zOrigin, size.x, size.z, cells);
-						
-						cells = null;
+					/* Make origin adjustments for negative room sizes */
+					if (mouseData.currentPosition.x < mouseData.originPosition.x) {
+						this.temporaryOrigin.x = mouseData.currentPosition.x;
 					}
 					
-				}
-
-				wasDown = false;
-			}
-		}
-
-		public void CreateRooms() {
-			var bounds = rooms[0].bounds;
-
-			// First, find the bounding box encasing all rooms
-			foreach (RoomDefinition room in rooms) {
-				bounds.xMin = room.bounds.xMin < bounds.xMin ? room.bounds.xMin : bounds.xMin;
-				bounds.xMax = room.bounds.xMax > bounds.xMax ? room.bounds.xMax : bounds.xMax;
-
-				bounds.yMin = room.bounds.yMin < bounds.yMin ? room.bounds.yMin : bounds.yMin;
-				bounds.yMax = room.bounds.yMax > bounds.yMax ? room.bounds.yMax : bounds.yMax;
-			}
-
-			Logger.Info("CreateRooms", "Global bounds found at {0}", bounds.ToString());
-
-			CellType[,] finalCells = new CellType[(int)bounds.width, (int)bounds.height];
-
-			foreach (RoomDefinition room in rooms) {
-
-				for (int x = 0; x < room.cells.GetLength(0); x++) {
-					for (int z = 0; z < room.cells.GetLength(1); z++) {
-						finalCells[(int)(room.bounds.xMin - bounds.xMin + x), (int)(room.bounds.yMin - bounds.yMin + z)] = room.cells[x, z];
+					if (mouseData.currentPosition.z < mouseData.originPosition.z) {
+						this.temporaryOrigin.z = mouseData.currentPosition.z;
 					}
+
+					mouseData.lastPosition = mouseData.currentPosition;
 				}
+			} else if (mouseData.isPressed && this.temporaryCells != null) {
+
+				if (temporarySize.x != 0 && temporarySize.z != 0) {
+					SetCells(this.temporaryOrigin, this.temporaryCells);
+				}
+
+				mouseData.isPressed = false;
+				this.temporaryCells = null;
 			}
-
-			this.rooms.Clear();
-
-			this.cells = finalCells;
-			this.finished = true;
-		}
-
-		public void Reset() {
-			this.rooms.Clear();
-			this.cells = null;
-
-			Logger.Info("Reset", "Reset room drawing");
-		}
-
-		private void AddTemporaryRoom(int x, int y, int width, int length, CellType[,] cells) {
-			rooms.Add(new RoomDefinition() {
-				bounds = new Rect(x, y, width, length),
-				cells = cells
-			});
-
-			Logger.Info("AddRoom", "Temporary room added at {0} {1}", x.ToString(), y.ToString());
 		}
 	}
 
