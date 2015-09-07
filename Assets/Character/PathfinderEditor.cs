778813742274ab2b72957f5e962ae59b0219a23f
Philipp Christoph <philipp.christoph@dm.de>
2015-09-07 09:22:37 +0200

    feat(pathfinder): implement inspector for pathfinder (allows step-by-step execution)

---
+++ Assets/Character/PathfinderEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using SpaceStation;
+using UnityEditor;
+using SpaceStation.Character;
+
+namespace SpaceStation {
+
+	[CustomEditor(typeof(Pathfinder))]
+	public class PathfinderEditor : Editor {
+
+		public override void OnInspectorGUI() {
+			var pathfinder = (Pathfinder) target;
+
+			pathfinder.PerformSteps = EditorGUILayout.Toggle("Step-by-Step", pathfinder.PerformSteps);
+
+			if (GUILayout.Button("Next step") && pathfinder.PerformSteps) {
+				pathfinder.ContinueSteps = true;
+			}
+		}
+	}
+
+}
